6db2bcdcc84371b9ddd50317335634cb20a09aed
Badal Kainth <badalkainth06@gmail.com>
2025-03-06 16:17:16 +0530

    fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,16 +11,21 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log('MongoDB Connection Error:', err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Connection Error:", err));
 
 // Routes
-const taskRoutes = require('./routes/taskRoutes');
-app.use('/tasks', taskRoutes);
+const taskRoutes = require("./routes/taskRoutes");
+app.use("/tasks", taskRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 // Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
